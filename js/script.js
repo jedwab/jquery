afea464f5bc5f0fdae486be682fcd102f3a0bfe7
Justyna Bobrowska <justyna.bobrowska88@gmail.com>
2018-01-15 17:10:00 +0100

    add buttons

--- js/script.js
+++ js/script.js
@@ -1,25 +1,46 @@
-
 $(function(){
-	var carouselList = $("#carousel ul");
-	setInterval(changeSlide, 3000);
+});
 
-	function changeSlide(){
-		carouselList.animate({'marginLeft':0}, 1200, moveFirstSlide);
-<<<<<<< HEAD
-		}
-=======
+var carouselList = $("#carousel ul");
+var carouselChangeTimer;
+
+setInterval(changeSlide, 5000);
+
+$("#leftArrow").click(function(){
+	changeSlide('leftDirection');
+	setInterval(5000);
+});
+
+$("#rightArrow").click(function(){		
+	changeSlide('rightDirection');
+	setInterval(5000);
+	
+});
+
+function changeSlide(animationDirection){
+	if (animationDirection == 'rightDirection'){
+		carouselList.animate({'marginLeft':-400}, 1200, moveRightSide);
 	}
->>>>>>> 6d07a023a0888c6bf5b607275c9deab4743b5c0c
+	else if (animationDirection == 'leftDirection'){
+		carouselList.animate({'marginLeft':0}, 1200, moveLeftSide);
+	}
+	else{
+		carouselList.animate({'marginLeft':0}, 1200, moveLeftSide);	
+	}
+}
 
-	function moveFirstSlide(){
+function moveLeftSide(){
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		firstItem.before(lastItem);
 		carouselList.css({marginLeft:-400});
-<<<<<<< HEAD
-		}
-});
-=======
-	}
-});
->>>>>>> 6d07a023a0888c6bf5b607275c9deab4743b5c0c
+}
+
+function moveRightSide(){
+	var firstItem = carouselList.find("li:first");
+	var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+}
+
+
