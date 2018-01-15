8c3e88e4bc2217c134e738d395e178310c90f78d
jedwab <34577105+jedwab@users.noreply.github.com>
2018-01-15 17:15:17 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,42 @@
 $(function(){
-	var carouselList = $("#carousel ul");
-	setInterval(changeSlide, 3000);
+});
 
-	function changeSlide(){
-		carouselList.animate({'marginLeft':0}, 1200, moveFirstSlide);
+var carouselList = $("#carousel ul");
+setInterval(changeSlide, 5000);
+
+$("#leftArrow").click(function(){
+	changeSlide('leftDirection');
+	setInterval(5000);
+});
+
+$("#rightArrow").click(function(){		
+	changeSlide('rightDirection');
+	setInterval(5000);
+	
+});
+
+function changeSlide(animationDirection){
+	if (animationDirection == 'rightDirection'){
+		carouselList.animate({'marginLeft':-400}, 1200, moveRightSide);
 	}
+	else if (animationDirection == 'leftDirection'){
+		carouselList.animate({'marginLeft':0}, 1200, moveLeftSide);
+	}
+	else{
+		carouselList.animate({'marginLeft':0}, 1200, moveLeftSide);	
+	}
+}
 
-	function moveFirstSlide(){
+function moveLeftSide(){
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		firstItem.before(lastItem);
 		carouselList.css({marginLeft:-400});
-	}
-});
+}
 
+function moveRightSide(){
+	var firstItem = carouselList.find("li:first");
+	var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+}
