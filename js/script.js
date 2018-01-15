e079ab4ac5eca9b03a7f01391c6fd75dfc50ff8c
jedwab <34577105+jedwab@users.noreply.github.com>
2018-01-15 01:58:09 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,16 @@
-
 $(function(){
 	var carouselList = $("#carousel ul");
 	setInterval(changeSlide, 3000);
 
 	function changeSlide(){
 		carouselList.animate({'marginLeft':0}, 1200, moveFirstSlide);
-<<<<<<< HEAD
-		}
-=======
 	}
->>>>>>> 6d07a023a0888c6bf5b607275c9deab4743b5c0c
 
 	function moveFirstSlide(){
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		firstItem.before(lastItem);
 		carouselList.css({marginLeft:-400});
-<<<<<<< HEAD
-		}
-});
-=======
 	}
 });
->>>>>>> 6d07a023a0888c6bf5b607275c9deab4743b5c0c
+
