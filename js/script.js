9962a78591ec4375b5738361b5cd1a3582d11c51
Justyna Bobrowska <justyna.bobrowska88@gmail.com>
2018-01-15 01:33:09 +0100

    clean code

--- js/script.js
+++ js/script.js
@@ -1,15 +1,16 @@
+
 $(function(){
 	var carouselList = $("#carousel ul");
 	setInterval(changeSlide, 3000);
 
-		function changeSlide(){
-	 		carouselList.animate({'marginLeft':0}, 1200, moveFirstSlide);
+	function changeSlide(){
+		carouselList.animate({'marginLeft':0}, 1200, moveFirstSlide);
 		}
 
-		function moveFirstSlide(){
-	        var firstItem = carouselList.find("li:first");
-	        var lastItem = carouselList.find("li:last");
-	        firstItem.before(lastItem);
-	        carouselList.css({marginLeft:-400});
-    	}
+	function moveFirstSlide(){
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft:-400});
+		}
 });
